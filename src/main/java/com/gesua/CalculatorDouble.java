03a90770bfd50a099422fd07dea5218a41b84679
Stanislav Bytsko <stanislav.bytsko@thomascookonline.com>
2017-04-12 22:35:54 +0300

    Move methods

--- src/main/java/com/gesua/CalculatorDouble.java
+++ src/main/java/com/gesua/CalculatorDouble.java
@@ -138,6 +138,229 @@ public class CalculatorDouble {
             v = v(dPar, popar, Fvn_Fvodu);
         }
 
+
+        //Расчеты нормализации
+        //Для nTrubok
+        private double normNtrub(double F, double Ltrub, double dRaschetTeploobTrubki) {
+            return F / PI / dRaschetTeploobTrubki / Ltrub;
+        }
+
+        private double ploshadEkvivalentnoyPoverhnosti(double dRaschetTeploobTrubki, double Ltrub, double nTrubok) {
+            return PI * dRaschetTeploobTrubki * Ltrub * nTrubok * 0.96;
+        }
+
+        private double tStenki(double T1par, double q, double alphaGorizontSkorostnaya) {
+            return (T1par - 5) - q / alphaGorizontSkorostnaya;
+        }
+
+        private double qNorm(double k, double dTser) {
+            return k * dTser;
+        }
+
+        //Общие+вода
+        private double dvn(double dnar, double tolshinaStenki, double bZagr, double bNaYjim) {
+            return dnar - (tolshinaStenki * TWO) - (bZagr * TWO) - (bNaYjim * TWO);
+        }
+
+        private double dRaschetTeploobTrubki(double dvn, double dnar) {
+            return (dvn + dnar) / 2;
+        }
+
+        private double fvn(double dvn) {
+            return PI * dvn * dvn / FOUR;
+        }
+
+        private double fnar(double dnar) {
+            return PI * dnar * dnar / FOUR;
+        }
+
+        private double fnarTrubok(double fnar, double nTrubok) {
+            return fnar * nTrubok;
+        }
+
+        private double Fvn(double Dvn) {
+            return Dvn * Dvn * PI / FOUR;
+        }
+
+        private double fvnNTrubok(double fvn, double nTrubok) {
+            return fvn * nTrubok;
+        }
+
+
+        private double nyuT2ser(double T2ser) {
+            return 0.000000556 + (0.000000478 - 0.000000556) / (60 - 50) * (T2ser - 50);
+        }
+
+        private double pr(double T2ser) {
+            return 3.54 + (2.93 - 3.54) / (60 - 50) * (T2ser - 50);
+        }
+
+        private double lamda(double T2ser) {
+            return Math.pow(10, -2) * (64.8 + (65.9 - 64.8) / (60 - 50) * (T2ser - 50));
+        }
+
+        private double v1trubka(double G, double fvn, double po) {
+            return G / fvn / po;
+        }
+
+        private double vNtrubok(double v1trubka, double nTrubok) {
+            return v1trubka / nTrubok;
+        }
+
+        private double Re(double vNtrubok, double dvn, double Nyu) {
+            return vNtrubok * dvn / Nyu;
+        }
+
+        private double reRuflen(double Re, double koefRuglen) {
+            return Re * koefRuglen;
+        }
+
+        private double nu(double Re, double pr) {
+            return 0.021 * Math.pow(Re, 0.8) * Math.pow(pr, 0.43);
+        }
+
+        private double nuRuflen(double reRuflen, double pr) {
+            return 0.021 * Math.pow(reRuflen, 0.8) * Math.pow(pr, 0.43);
+        }
+
+        private double alpha(double nu, double lamda, double dvn) {
+            return nu * lamda / dvn;
+        }
+
+        private double alphaRuflen(double nuRuflen, double lamda, double dvn) {
+            return nuRuflen * lamda / dvn;
+        }
+
+        private double kPoRovnoy(double alpha, double tolshinaStenki, double lyamdaStali, double bZagr, double alphaGorizontSkorostnaya) {
+            return 1 / (1 / alpha + tolshinaStenki / lyamdaStali + bZagr / 1.16 + 1 / alphaGorizontSkorostnaya);
+        }
+
+        private double alphaGorizontSkorostnaya(double T1par, double dnar) {
+            return (4950 + 58.5 * 97.66 - (0.18 * Math.pow(97.66, 2))) / Math.pow(((T1par - 89.5) * 3 * dnar), 0.25);
+        }
+
+        private double t2sred(double T2vuh, double t2sht) {
+            return (T2vuh + t2sht) / 2;
+        }
+
+        private double dTb(double T1par, double t2sht) {
+            return T1par - t2sht;
+        }
+
+        private double dTm(double T1par, double T2vuh) {
+            return T1par - T2vuh;
+        }
+
+        private double dTser(double dTb, double dTm) {
+            return (dTb - dTm) / Math.log(dTb / dTm);
+        }
+
+        private double T2ser(double T1par, double dTser) {
+
+            return T1par - dTser;
+        }
+
+        private double q(double G, double cp, double T2vuh, double t2sht) {
+            return G * cp * (T2vuh - t2sht) * THOUSAND;
+        }
+
+        private double qPo45min(double cp, double T2vuh, double t2sht) {
+            return 1000 * 60 / 45 * cp * (T2vuh - t2sht) / 3600 * THOUSAND;
+        }
+
+        private double k(double alphaRuflen, double tolshinaStenki, double bzagr, double alphaGorizontSkorostnaya) {
+            return 1 / (1 / alphaRuflen + tolshinaStenki / 16.2 + bzagr / 1.16 + 1 / alphaGorizontSkorostnaya);
+        }
+
+        private double F(double q, double dTser, double k) {
+            return q / dTser / k;
+        }
+
+        private double fPo45min(double qPo45min, double dTser, double k) {
+            return qPo45min / dTser / k;
+        }
+
+        //need
+        private double Fvodu(double fnar, double nTrubok) {
+
+            return fnar * nTrubok;
+        }
+
+        private double Fvn_Fvodu(double Fvn, double fnar, double nTrubok) {
+            return Fvn - fnar * nTrubok;
+        }
+
+        private double Dekv(double Fvn_Fvodu) {
+            return Math.pow((4 * Fvn_Fvodu / PI), 0.5);
+        }
+
+        private double dekv(double Fvodu) {
+            return Math.pow((4 * Fvodu / PI), 0.5);
+        }
+
+        private double fvnVodu(double dvn, double nTrubok) {
+            return Math.pow(dvn, 2) * PI / 4 * nTrubok;
+        }
+
+        private double dekvVnTrubok(double fvn) {
+            return Math.pow((fvn * 4 / PI), 0.5);
+        }
+
+//    Пар
+
+        private double dPar(double Q, double r) {
+            return Q / r / 1000;
+        }
+
+        private double v(double Dpar, double poPar, double Fvn_Fvodu) {
+            return Dpar / poPar / Fvn_Fvodu;
+        }
+
+        private double RePar(double v, double Dekv, double NuPar) {
+            return v * Dekv / NuPar;
+        }
+
+        private double nuPar(double Re, double PrPar) {
+            return 0.021 * Math.pow(Re, 0.8) * Math.pow(PrPar, 0.43);
+        }
+
+        private double AlphaPar(double nuPar, double lamdaKond, double Dekv) {
+            return nuPar * lamdaKond / Dekv;
+        }
+
+        private double A(double lamdaKond, double r, double poKond, double poPar, double NuPar) {
+            return Math.pow((Math.pow(lamdaKond, 3) * 9.81 * r * 1000 * (poKond - poPar) / NuPar), 0.25);
+        }
+
+        private double AlphaGorKondNepodPar(double A, double dnar, double T1par, double Tstenki1) {
+            return 0.728 * A / Math.pow(dnar * (T1par - Tstenki1), 0.25);
+        }
+
+        //    Сопротивление по воде
+        private double reEkv(double vNtrubok, double dekvVnTrubok, double nu) {
+            return vNtrubok * dekvVnTrubok / nu;
+        }
+
+        private double soprLamda(double dEkv, double reEkv) {
+            return 0.11 * Math.pow((1 / dEkv + 68 / reEkv), 0.25);
+        }
+
+        private double dPot(double soprLamda, double Ltrub, double dekvVnTrubok, double vNtrubok, double po) {
+            return (soprLamda * Ltrub / dekvVnTrubok * Math.pow(vNtrubok, 2) / 2 * po) / 1000;
+        }
+
+        private double ksiSujeniya(double fvnVodu, double sorpF) {
+            return 0.5 * (1 - fvnVodu / sorpF);
+        }
+
+        private double dpSujeniya(double ksiSujeniya, double vNtrubok, double po) {
+            return (ksiSujeniya * Math.pow(vNtrubok, 2) * po / 2) / 1000;
+        }
+
+        private double dpTotal(double dPot, double dpSujeniya) {
+            return dPot + dpSujeniya;
+        }
+
         @Override
         public String toString() {
             return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
@@ -157,227 +380,5 @@ public class CalculatorDouble {
 
         System.out.println(report);
 
-    }
-
-    //Расчеты нормализации
-    //Для nTrubok
-    private static double normNtrub(double F, double Ltrub, double dRaschetTeploobTrubki) {
-        return F / PI / dRaschetTeploobTrubki / Ltrub;
-    }
-
-    private static double ploshadEkvivalentnoyPoverhnosti(double dRaschetTeploobTrubki, double Ltrub, double nTrubok) {
-        return PI * dRaschetTeploobTrubki * Ltrub * nTrubok * 0.96;
-    }
-
-    private static double tStenki(double T1par, double q, double alphaGorizontSkorostnaya) {
-        return (T1par - 5) - q / alphaGorizontSkorostnaya;
-    }
-
-    private static double qNorm(double k, double dTser) {
-        return k * dTser;
-    }
-
-    //Общие+вода
-    private static double dvn(double dnar, double tolshinaStenki, double bZagr, double bNaYjim) {
-        return dnar - (tolshinaStenki * TWO) - (bZagr * TWO) - (bNaYjim * TWO);
-    }
-
-    private static double dRaschetTeploobTrubki(double dvn, double dnar) {
-        return (dvn + dnar) / 2;
-    }
-
-    private static double fvn(double dvn) {
-        return PI * dvn * dvn / FOUR;
-    }
-
-    private static double fnar(double dnar) {
-        return PI * dnar * dnar / FOUR;
-    }
-
-    private static double fnarTrubok(double fnar, double nTrubok) {
-        return fnar * nTrubok;
-    }
-
-    private static double Fvn(double Dvn) {
-        return Dvn * Dvn * PI / FOUR;
-    }
-
-    private static double fvnNTrubok(double fvn, double nTrubok) {
-        return fvn * nTrubok;
-    }
-
-
-    private static double nyuT2ser(double T2ser) {
-        return 0.000000556 + (0.000000478 - 0.000000556) / (60 - 50) * (T2ser - 50);
-    }
-
-    private static double pr(double T2ser) {
-        return 3.54 + (2.93 - 3.54) / (60 - 50) * (T2ser - 50);
-    }
-
-    private static double lamda(double T2ser) {
-        return Math.pow(10, -2) * (64.8 + (65.9 - 64.8) / (60 - 50) * (T2ser - 50));
-    }
-
-    private static double v1trubka(double G, double fvn, double po) {
-        return G / fvn / po;
-    }
-
-    private static double vNtrubok(double v1trubka, double nTrubok) {
-        return v1trubka / nTrubok;
-    }
-
-    private static double Re(double vNtrubok, double dvn, double Nyu) {
-        return vNtrubok * dvn / Nyu;
-    }
-
-    private static double reRuflen(double Re, double koefRuglen) {
-        return Re * koefRuglen;
-    }
-
-    private static double nu(double Re, double pr) {
-        return 0.021 * Math.pow(Re, 0.8) * Math.pow(pr, 0.43);
-    }
-
-    private static double nuRuflen(double reRuflen, double pr) {
-        return 0.021 * Math.pow(reRuflen, 0.8) * Math.pow(pr, 0.43);
-    }
-
-    private static double alpha(double nu, double lamda, double dvn) {
-        return nu * lamda / dvn;
-    }
-
-    private static double alphaRuflen(double nuRuflen, double lamda, double dvn) {
-        return nuRuflen * lamda / dvn;
-    }
-
-    private static double kPoRovnoy(double alpha, double tolshinaStenki, double lyamdaStali, double bZagr, double alphaGorizontSkorostnaya) {
-        return 1 / (1 / alpha + tolshinaStenki / lyamdaStali + bZagr / 1.16 + 1 / alphaGorizontSkorostnaya);
-    }
-
-    private static double alphaGorizontSkorostnaya(double T1par, double dnar) {
-        return (4950 + 58.5 * 97.66 - (0.18 * Math.pow(97.66, 2))) / Math.pow(((T1par - 89.5) * 3 * dnar), 0.25);
-    }
-
-    private static double t2sred(double T2vuh, double t2sht) {
-        return (T2vuh + t2sht) / 2;
-    }
-
-    private static double dTb(double T1par, double t2sht) {
-        return T1par - t2sht;
-    }
-
-    private static double dTm(double T1par, double T2vuh) {
-        return T1par - T2vuh;
-    }
-
-    private static double dTser(double dTb, double dTm) {
-        return (dTb - dTm) / Math.log(dTb / dTm);
-    }
-
-    private static double T2ser(double T1par, double dTser) {
-
-        return T1par - dTser;
-    }
-
-    private static double q(double G, double cp, double T2vuh, double t2sht) {
-        return G * cp * (T2vuh - t2sht) * THOUSAND;
-    }
-
-    private static double qPo45min(double cp, double T2vuh, double t2sht) {
-        return 1000 * 60 / 45 * cp * (T2vuh - t2sht) / 3600 * THOUSAND;
-    }
-
-    private static double k(double alphaRuflen, double tolshinaStenki, double bzagr, double alphaGorizontSkorostnaya) {
-        return 1 / (1 / alphaRuflen + tolshinaStenki / 16.2 + bzagr / 1.16 + 1 / alphaGorizontSkorostnaya);
-    }
-
-    private static double F(double q, double dTser, double k) {
-        return q / dTser / k;
-    }
-
-    private static double fPo45min(double qPo45min, double dTser, double k) {
-        return qPo45min / dTser / k;
-    }
-
-    //need
-    private static double Fvodu(double fnar, double nTrubok) {
-
-        return fnar * nTrubok;
-    }
-
-    private static double Fvn_Fvodu(double Fvn, double fnar, double nTrubok) {
-        return Fvn - fnar * nTrubok;
-    }
-
-    private static double Dekv(double Fvn_Fvodu) {
-        return Math.pow((4 * Fvn_Fvodu / PI), 0.5);
-    }
-
-    private static double dekv(double Fvodu) {
-        return Math.pow((4 * Fvodu / PI), 0.5);
-    }
-
-    private static double fvnVodu(double dvn, double nTrubok) {
-        return Math.pow(dvn, 2) * PI / 4 * nTrubok;
-    }
-
-    private static double dekvVnTrubok(double fvn) {
-        return Math.pow((fvn * 4 / PI), 0.5);
-    }
-
-//    Пар
-
-    private static double dPar(double Q, double r) {
-        return Q / r / 1000;
-    }
-
-    private static double v(double Dpar, double poPar, double Fvn_Fvodu) {
-        return Dpar / poPar / Fvn_Fvodu;
-    }
-
-    private static double RePar(double v, double Dekv, double NuPar) {
-        return v * Dekv / NuPar;
-    }
-
-    private static double nuPar(double Re, double PrPar) {
-        return 0.021 * Math.pow(Re, 0.8) * Math.pow(PrPar, 0.43);
-    }
-
-    private static double AlphaPar(double nuPar, double lamdaKond, double Dekv) {
-        return nuPar * lamdaKond / Dekv;
-    }
-
-    private static double A(double lamdaKond, double r, double poKond, double poPar, double NuPar) {
-        return Math.pow((Math.pow(lamdaKond, 3) * 9.81 * r * 1000 * (poKond - poPar) / NuPar), 0.25);
-    }
-
-    private static double AlphaGorKondNepodPar(double A, double dnar, double T1par, double Tstenki1) {
-        return 0.728 * A / Math.pow(dnar * (T1par - Tstenki1), 0.25);
-    }
-
-    //    Сопротивление по воде
-    private static double reEkv(double vNtrubok, double dekvVnTrubok, double nu) {
-        return vNtrubok * dekvVnTrubok / nu;
-    }
-
-    private static double soprLamda(double dEkv, double reEkv) {
-        return 0.11 * Math.pow((1 / dEkv + 68 / reEkv), 0.25);
-    }
-
-    private static double dPot(double soprLamda, double Ltrub, double dekvVnTrubok, double vNtrubok, double po) {
-        return (soprLamda * Ltrub / dekvVnTrubok * Math.pow(vNtrubok, 2) / 2 * po) / 1000;
-    }
-
-    private static double ksiSujeniya(double fvnVodu, double sorpF) {
-        return 0.5 * (1 - fvnVodu / sorpF);
-    }
-
-    private static double dpSujeniya(double ksiSujeniya, double vNtrubok, double po) {
-        return (ksiSujeniya * Math.pow(vNtrubok, 2) * po / 2) / 1000;
-    }
-
-    private static double dpTotal(double dPot, double dpSujeniya) {
-        return dPot + dpSujeniya;
     }
 }
